a2a4e7a9a1870328446e0acab232da5a1da178eb
Marcus Richmond <marcu5@countermail.com>
2018-03-23 12:08:42 -0400

    added testing

--- server.js
+++ server.js
@@ -3,9 +3,11 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const app = express();
+
 const blogPostsRouter = require('./blogPostsRouter');
 
-const app = express();
+app.use(morgan('common'));
 
 app.use(express.static('public'));
 
@@ -17,3 +19,40 @@ app.use('/blog-posts', blogPostsRouter);
 
 app.listen(process.env.PORT || 3000, () => console.log(
   `Your app is listening on port ${process.env.PORT || 3000}`));
+
+  function runServer() {
+    const port = process.env.PORT || 3000;
+    return new Promise((resolve, reject) => {
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve(server);
+      }).on('error', err => {
+        reject(err)
+      });
+    });
+  }
+
+  // like `runServer`, this function also needs to return a promise.
+  // `server.close` does not return a promise on its own, so we manually
+  // create one.
+  function closeServer() {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          reject(err);
+          // so we don't also call `resolve()`
+          return;
+        }
+        resolve();
+      });
+    });
+  }
+
+  // if server.js is called directly (aka, with `node server.js`), this block
+  // runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
+  if (require.main === module) {
+    runServer().catch(err => console.error(err));
+  };
+
+  module.exports = {app, runServer, closeServer};
